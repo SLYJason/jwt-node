2cbd8a5eb6ff6941553cfd56e0bc4ff86df2b292
Luyi Song <songly91.6.1@gmail.com>
2018-06-19 21:58:03 -0400

    post is working

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ app.get('/api', (req, res) => {
     res.json({
         message: 'Welcome to the API'
     });
-    console.log(req);
-    console.log(res);
 });
+
+app.post('/api/posts', (req, res) => {
+    res.json({
+        message: 'Post Created...'
+    })
+})
 
 app.listen(4200, () => console.log('server started on port 4200'));
