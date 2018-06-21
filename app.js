2b4386b18e01291f6e288467ff3d44865bcfd149
Luyi Song <songly91.6.1@gmail.com>
2018-06-20 23:06:09 -0400

    auth works

--- app.js
+++ app.js
@@ -3,18 +3,31 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+const secretKey = 'secretKey';
+
+// Welcome page
 app.get('/api', (req, res) => {
     res.json({
         message: 'Welcome to the API'
     });
 });
 
-app.post('/api/posts', (req, res) => {
-    res.json({
-        message: 'Post Created...'
-    })
+// Test Post method
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, secretKey, (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        }
+        else {
+            res.json({
+                message: 'Posts Created...',
+                authData
+            })
+        }
+    });
 });
 
+// Get auth token
 app.post('/api/login', (req, res) => {
     // Mock user
     const user = {
@@ -22,12 +35,27 @@ app.post('/api/login', (req, res) => {
         name: "Luyi Song",
         company: "Comcast"
     }
-    token = jwt.sign(user, 'foo', (err, token) => {
+    token = jwt.sign({user}, secretKey, {expiresIn: '10h'}, (err, token) => {
         res.json({
             token
         });
-        console.error('Error', err);
     });
 });
+
+// Format token
+// Authorization: Bearer <token>
+
+// Verify token
+function verifyToken(req, res, next) {
+    // Get auth header
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader !== 'undefined') {
+        req.token = bearerHeader.split(' ')[1];
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+}
 
 app.listen(4200, () => console.log('server started on port 4200'));
